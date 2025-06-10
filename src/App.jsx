d778efc84813a92581c7d287f081a30809403050
quophiBonney <quophiclef@gmail.com>
2025-06-10 13:54:06 +0000

    added gtag

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
-import CustomNavbar from './components/Navbar'
-import Footer from './components/Footer'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Contact from './pages/Contact'
-import Services from './pages/Services'
-import About from './pages/About'
-
+import React from "react";
+import CustomNavbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Services from "./pages/Services";
+import About from "./pages/About";
+import ReactGA from "react-ga4";
 const App = () => {
   return (
     <>
-    <CustomNavbar/>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/contact" element={<Contact/>}/>
-      <Route path="/services" element={<Services/>}/>
-      <Route path="/about-us" element={<About/>}/>
-    </Routes>
-    <Footer/>
+      {ReactGA.initialize("AW-17178493997")}
+      <CustomNavbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/about-us" element={<About />} />
+      </Routes>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
